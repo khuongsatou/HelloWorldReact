fa87d67bf7bdf60f7078eaf78f45743ccc0a25bd
khuongsatou <yoshi240499@gmail.com>
2020-01-09 19:47:51 +0700

    Update StatusBar and Hidden Warning

--- Hello_World/App.js
+++ Hello_World/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+// import React from 'react';
+import {
+  YellowBox,
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  StatusBar
+} from 'react-native';
 import { Avatar } from "./src/Components";
-export default function App() {
-  return (
-    <View>
-      <Avatar
-        width={50}
-        height={50}
-        borderRadius={50 / 2}
-        backgroundColor={"red"}
-      />
-      <Avatar
-        width={50}
-        height={50}
-        borderRadius={50 / 2}
-        backgroundColor={"blue"}
-      />
+import { Style } from "./src/comon";
 
-    </View>
+YellowBox.ignoreWarnings(['Warning:...']);
+console.disableYellowBox = true;
+console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
+// const rectangle = {
+//   width: 200,
+//   height: 300,
+//   backgroundColor:'blue'
+// };
 
-  );
+export default class Demo1 extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Hello ConStructor");
+  }
+
+  componentWillMount(){
+    console.log("Hello WillMount");
+  }
+
+  render() {
+    console.log("Hello Render");
+    return (
+      <View>
+        
+        <StatusBar hidden={true} />
+        <View style={{
+          marginTop: 20,
+          width: 50,
+          height: 50,
+          backgroundColor: 'red'
+        }}>
+        </View>
+        <View style={Style.rectangle} />
+        <View style={styles.header, styles.background} />
+
+
+      </View>
+    );
+  }
+
+  componentDidMount(){
+    console.log("Hello DidMount");
+  }
+
+
 }
+
+AppRegistry.registerComponent('Demo1', () => Demo1);
+
+// export default function App() {
+//   return (
+//     <View>
+
+//       <View style={{
+//         marginTop:20,
+//         width: 50,
+//         height: 50,
+//         backgroundColor: 'red'
+//       }}>
+//         </View>
+//         <View style={Style.rectangle}/>
+//         <View style={styles.header,styles.background}/>
+
+
+//     </View>
+
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -28,5 +86,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  header: {
+    width: 500,
+    height: 300,
+    borderWidth: 1,
+    marginTop: 4
+  },
+  background: {
+    borderColor: 'green'
+  }
 });
---
+++ Hello_World/src/comon/Style.js
@@ -0,0 +1,9 @@
+const Style = {
+    rectangle : {
+        width: 200,
+        height: 300,
+        backgroundColor:'blue'
+    }
+};
+
+export default Style;
