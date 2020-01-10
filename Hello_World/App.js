0288dd528971647aa451ce78f99bbd17a798c745
khuongsatou <yoshi240499@gmail.com>
2020-01-10 17:25:16 +0700

    Update const[] get set and useState

--- Hello_World/App.js
+++ Hello_World/App.js
@@ -8,12 +8,18 @@ import {
 
 export default function App(){
   const [name,setName] = useState('Khuong');
+  const [person,setPerson] = useState({
+    name:'mario',
+    age:40
+  });
   const clickHandler = () =>{
     setName('Thang Khac');
+    setPerson({name:'Mario 2' ,age:45});
   }
   return (
     <View style={styles.container}>
       <Text>My Name is {name}</Text>
+<Text>His name is {person.name} and his age is {person.age}</Text>
       <Text></Text>
       <View style={styles.buttonContainer}>
         <Button title='update State' onPress={clickHandler}/>
